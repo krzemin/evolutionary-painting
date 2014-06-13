41702a0670b3734a0a99546346b0a9e09eff5be7
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-06-13 16:56:37 +0200

    slight refactoring; using polygons instead of ovals

--- src/main/scala/EvolutionaryPainting.scala
+++ src/main/scala/EvolutionaryPainting.scala
@@ -1,5 +1,5 @@
 import java.awt.image.BufferedImage
-import java.awt.Color
+import java.awt.{Graphics, Color}
 import java.io.{PrintStream, File}
 import javax.imageio.ImageIO
 import scala.util.Random
@@ -9,8 +9,8 @@ object EvolutionaryPainting extends App {
   val rand = new Random()
 
   val iterations = 1000000
-  val rRatio = 0.04
-  val initCircles = 5
+  val rRatio = 0.8
+  val initShapes = 1
   val addFreq = 0.22
   val delFreq = 0.21
   val mutFreq = 0.02
@@ -18,59 +18,112 @@ object EvolutionaryPainting extends App {
   val mutRRatio = 0.45
   val mutColorRatio = 0.15
   val mutAlphaRatio = 0.1
-  val minAlpha = 60
-  val maxAlpha = 180
-  val maxCircles = 5000
+  val minAlpha = 10
+  val maxAlpha = 60
+  val maxShapes = 300
+  val minPoly = 3
+  val maxPoly = 6
 
   val img: BufferedImage = ImageIO.read(new File("images/rynek2.png"))
   val (w, h) = (img.getWidth, img.getHeight)
   val diagonal = math.sqrt(w*w + h*h).toInt / 4 * 3
 
-  case class Oval(x: Int, y: Int, r1: Int, r2: Int, c: Color)
+  trait Shape {
+    val c: Color
 
-  object Oval {
-    def apply(): Oval = {
-      val r1 = 1 + rand.nextInt((diagonal * rRatio).toInt)
-      val r2 = 1 + rand.nextInt((diagonal * rRatio).toInt)
-      val x = rand.nextInt(w - r1)
-      val y = rand.nextInt(h - r2)
+    def draw(g: Graphics): Unit
+    def mutated: Shape
+  }
+
+//  case class Oval(x: Int, y: Int, r1: Int, r2: Int, c: Color) extends Shape {
+//    def draw(g: Graphics) {
+//      g.setColor(c)
+//      g.fillOval(x, y, r1, r2)
+//    }
+//    def mutated: Oval = {
+//      val xMut = x + ((rand.nextGaussian() - 0.5) * r2 * mutCoordRatio).toInt
+//      val yMut = y + ((rand.nextGaussian() - 0.5) * r1 * mutCoordRatio).toInt
+//      val r1Mut = r1 //+ ((rand.nextGaussian() - 0.5) * circle.r * mutRRatio).toInt
+//      val r2Mut = r2
+//      val cMut = new Color(
+//        (c.getRed + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
+//        (c.getGreen + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
+//        (c.getBlue + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
+//        (c.getAlpha + ((rand.nextGaussian() - 0.5) * (maxAlpha - minAlpha) * mutAlphaRatio).toInt).max(minAlpha).min(maxAlpha)
+//      )
+//      copy(
+//        xMut.max(0).min(w - r1Mut),
+//        yMut.max(0).min(h - r2Mut),
+//        r1Mut.max(1),
+//        r2Mut.max(1),
+//        cMut)
+//    }
+//  }
+//
+//  object Oval {
+//    def random = {
+//      val rr1 = 1 + rand.nextInt((diagonal * rRatio).toInt)
+//      val rr2 = 1 + rand.nextInt((diagonal * rRatio).toInt)
+//      val rx = rand.nextInt(w - rr1)
+//      val ry = rand.nextInt(h - rr2)
+//      val rc = new Color(
+//        rand.nextInt(256),
+//        rand.nextInt(256),
+//        rand.nextInt(256),
+//        minAlpha + rand.nextInt(maxAlpha - minAlpha)
+//      )
+//      Oval(rx, ry, rr1, rr2, rc)
+//    }
+//  }
+
+
+  case class Polygon(xs: Array[Int], ys: Array[Int], c: Color) extends Shape {
+    def draw(g: Graphics) {
+      g.setColor(c)
+      g.fillPolygon(xs, ys, xs.length)
+    }
+    def mutated: Polygon = {
+      val xs2 = xs.map(x => x + ((rand.nextGaussian() - 0.5) * diagonal * mutCoordRatio).toInt)
+      val ys2 = ys.map(y => y + ((rand.nextGaussian() - 0.5) * diagonal * mutCoordRatio).toInt)
+      val cMut = new Color(
+        (c.getRed + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
+        (c.getGreen + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
+        (c.getBlue + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
+        (c.getAlpha + ((rand.nextGaussian() - 0.5) * (maxAlpha - minAlpha) * mutAlphaRatio).toInt).max(minAlpha).min(maxAlpha)
+      )
+      copy(xs2, ys2, cMut)
+    }
+  }
+
+  object Polygon {
+    def random = {
+      val n = rand.nextInt(maxPoly - minPoly) + minPoly
+      val xs = Array.fill(n)(rand.nextInt(w))
+      val ys = Array.fill(n)(rand.nextInt(h))
       val c = new Color(
         rand.nextInt(256),
         rand.nextInt(256),
         rand.nextInt(256),
         minAlpha + rand.nextInt(maxAlpha - minAlpha)
       )
-      Oval(x, y, r1, r2, c)
-    }
-    def mutate(circle: Oval): Oval = {
-      val xMut = circle.x + ((rand.nextGaussian() - 0.5) * circle.r2 * mutCoordRatio).toInt
-      val yMut = circle.y + ((rand.nextGaussian() - 0.5) * circle.r1 * mutCoordRatio).toInt
-      val r1Mut = circle.r1 //+ ((rand.nextGaussian() - 0.5) * circle.r * mutRRatio).toInt
-      val r2Mut = circle.r2
-      val cMut = new Color(
-        (circle.c.getRed + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
-        (circle.c.getGreen + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
-        (circle.c.getBlue + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
-        (circle.c.getAlpha + ((rand.nextGaussian() - 0.5) * (maxAlpha - minAlpha) * mutAlphaRatio).toInt).max(minAlpha).min(maxAlpha)
-      )
-      circle.copy(
-        xMut.max(0).min(w - r1Mut),
-        yMut.max(0).min(h - r2Mut),
-        r1Mut.max(1),
-        r2Mut.max(1),
-        cMut)
+      Polygon(xs, ys, c)
     }
   }
 
-  type DNA = Vector[Oval]
+
+
+  type DNA = Vector[Shape]
 
   object DNA {
-    def make: DNA = Vector.fill(initCircles)(Oval())
+    def makeOne: Shape = Polygon.random
+    def make: DNA = Vector.fill(initShapes)(makeOne)
     def randomDel(dna: DNA): DNA = dna.patch(rand.nextInt(dna.size), Nil, 1)
     def randomAdd(dna: DNA): DNA =
-      Oval() +: (if(dna.size < maxCircles) dna else randomDel(dna))
+      makeOne +: (if(dna.size < maxShapes) dna else randomDel(dna))
     def randomMut(dna: DNA): DNA =
-      dna.map(c => if(rand.nextDouble() < mutFreq) Oval.mutate(c) else c).toVector
+      dna.map(c => if(rand.nextDouble() < mutFreq) c.mutated else c).toVector
+    def randomShuffle(dna: DNA): DNA =
+      rand.shuffle(dna).toVector
 
     def draw(dna: DNA): BufferedImage = {
       val image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
@@ -78,8 +131,7 @@ object EvolutionaryPainting extends App {
       g.setColor(Color.BLACK)
       g.fillRect(0, 0, w, h)
       dna.foreach { circle =>
-        g.setColor(circle.c)
-        g.fillOval(circle.x, circle.y, circle.r1, circle.r2)
+        circle.draw(g)
       }
       image
     }
@@ -110,7 +162,7 @@ object EvolutionaryPainting extends App {
   for(iter <- 1 to iterations) {
     val strIter = "%06d".format(iter)
 
-    val funs: List[DNA => DNA] = List(DNA.randomAdd, DNA.randomDel, DNA.randomAdd, DNA.randomMut)
+    val funs: List[DNA => DNA] = List(DNA.randomShuffle, DNA.randomDel, DNA.randomAdd, DNA.randomMut)
     val imagesWithFitness = funs.par.map { f =>
       val child = f(parent)
       val image = DNA.draw(child)
