3e0361abe16b0543dbcf4b0945c8776124f0fc5b
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-06-13 13:58:57 +0200

    mutation without changing size of the circle; 3 types of mutation at once + identity = 4 children produced in every step

--- src/main/scala/EvolutionaryPainting.scala
+++ src/main/scala/EvolutionaryPainting.scala
@@ -9,18 +9,18 @@ object EvolutionaryPainting extends App {
   val rand = new Random()
 
   val iterations = 1000000
-  val rRatio = 0.15
+  val rRatio = 0.1
   val initCircles = 5
   val addFreq = 0.22
   val delFreq = 0.21
   val mutFreq = 0.02
   val mutCoordRatio = 0.33
   val mutRRatio = 0.45
-  val mutColorRatio = 0.04
+  val mutColorRatio = 0.15
   val mutAlphaRatio = 0.1
-  val minAlpha = 20
-  val maxAlpha = 120
-  val maxCircles = 1000
+  val minAlpha = 60
+  val maxAlpha = 180
+  val maxCircles = 10000
 
   val img: BufferedImage = ImageIO.read(new File("images/rynek2.png"))
   val (w, h) = (img.getWidth, img.getHeight)
@@ -44,7 +44,7 @@ object EvolutionaryPainting extends App {
     def mutate(circle: Circle): Circle = {
       val xMut = circle.x + ((rand.nextGaussian() - 0.5) * circle.r * mutCoordRatio).toInt
       val yMut = circle.y + ((rand.nextGaussian() - 0.5) * circle.r * mutCoordRatio).toInt
-      val rMut = circle.r + ((rand.nextGaussian() - 0.5) * circle.r * mutRRatio).toInt
+      val rMut = circle.r //+ ((rand.nextGaussian() - 0.5) * circle.r * mutRRatio).toInt
       val cMut = new Color(
         (circle.c.getRed + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
         (circle.c.getGreen + ((rand.nextGaussian() - 0.5) * 255 * mutColorRatio).toInt).max(0).min(255),
@@ -63,21 +63,13 @@ object EvolutionaryPainting extends App {
 
   object DNA {
     def make: DNA = Vector.fill(initCircles)(Circle())
-    def mutate(dna: DNA): DNA = {
-      val dna1 =
-        if(rand.nextDouble() < delFreq)
-          dna.patch(rand.nextInt(dna.size), Nil, 1)
-        else
-          dna
+    def randomDel(dna: DNA): DNA = dna.patch(rand.nextInt(dna.size), Nil, 1)
+    def randomAdd(dna: DNA): DNA =
+      Circle() +: (if(dna.size < maxCircles) dna else randomDel(dna))
 
-      val dna2 =
-        if(dna1.size < maxCircles && rand.nextDouble() < addFreq)
-          Circle() +: dna1
-        else
-          dna1
+    def randomMut(dna: DNA): DNA =
+      dna.par.map(c => if(rand.nextDouble() < mutFreq) Circle.mutate(c) else c).toVector
 
-      dna2.par.map(c => if(rand.nextDouble() < mutFreq) Circle.mutate(c) else c).toVector
-    }
     def draw(dna: DNA): BufferedImage = {
       val image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
       val g = image.getGraphics
@@ -109,21 +101,28 @@ object EvolutionaryPainting extends App {
   var parent = DNA.make
   val out = new PrintStream("plot.data")
   val pImg = DNA.draw(parent)
+  var parentFitness = DNA.fitness(pImg)
   out.println(s"000000 ${DNA.fitness(pImg)}")
   ImageIO.write(pImg, "png", new File(s"images/000000.png"))
 
   for(iter <- 1 to iterations) {
     val strIter = "%06d".format(iter)
 
-    val child = DNA.mutate(parent)
+    val funs: List[DNA => DNA] = List(x => x, DNA.randomDel, DNA.randomAdd, DNA.randomMut)
+    val imagesWithFitness = funs.par.map { f =>
+      val child = f(parent)
+      val image = DNA.draw(child)
+      val fitness = DNA.fitness(image)
+      (child, image, fitness)
+    }.toList
 
-    val List(parentImg, childImg) = List(parent, child).par.map(DNA.draw).toList
-    val List(parentF, childF) = List(parentImg, childImg).par.map(DNA.fitness).toList
+    val (bestChild, bestChildImg, bestChildFitness) = imagesWithFitness.sortBy(_._3).head
 
-    if(childF < parentF) {
-      out.println(s"$strIter $parentF ${parent.size}")
-      parent = child
-      ImageIO.write(childImg, "png", new File(s"images/$strIter.png"))
+    if(bestChildFitness < parentFitness) {
+      parentFitness = bestChildFitness
+      out.println(s"$strIter $parentFitness ${parent.size}")
+      parent = bestChild
+      ImageIO.write(bestChildImg, "png", new File(s"images/$strIter.png"))
     }
   }
 
